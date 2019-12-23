f519f4732aaf2c47b31d39b1d47fd36cf8065ddf
Administrator <admin@example.com>
2019-12-23 15:34:13 +0800

    comopser update

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+namespace yangkai\code\version;
+
+
+/**
+ * Created by PhpStorm.
+ * User: yangkai
+ * Date: 2019/12/17
+ * Time: 下午10:59
+ */
+
+class index
+{
+
+    /**
+     * 开启脚本入口
+     */
+    public function init()
+    {
+
+
+
+    }
+
+}
